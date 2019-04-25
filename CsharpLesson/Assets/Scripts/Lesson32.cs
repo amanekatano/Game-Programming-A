90f9ac3b3362f0da5f1bbb035e332f59b7189592
Tomohiko Mukai <contact@mukai-lab.org>
2019-04-25 09:45:48 +0900

    Lesson32base

---
+++ CsharpLesson/Assets/Scripts/Lesson32.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lesson32 : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    private void OnMouseEnter()
+    {
+        //Debug.Log("Enter");
+    }
+
+    private void OnMouseExit()
+    {
+        //Debug.Log("Exit");
+    }
+}
