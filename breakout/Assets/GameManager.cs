cf4893bddf1dfee947ec3a1690f3506e78e45053
Tomohiko Mukai <tmki@acm.org>
2019-05-09 14:06:29 +0900

    TODO: Game Manager

---
+++ breakout/Assets/GameManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject block; // Unity側で指定されるblock prefab
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        for () // 縦に並べる
+        {
+            for () // 横に並べる
+            {
+                Instantiate(block, new Vector3(???, ???, 0), Quaternion.identity);
+                // blockオブジェクトを指定した位置に生成する
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
