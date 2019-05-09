c3fb642bab776bd76aaf3ae7918d0ba3fcf0fd1c
Tomohiko Mukai <tmki@acm.org>
2019-05-09 12:52:04 +0900

    5/9午前分

---
+++ breakout/Assets/BallController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallController : MonoBehaviour
+{
+    Rigidbody2D rb;
+    public float speed = 10.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        rb.AddForce((Vector2.right + Vector2.down) * speed, ForceMode2D.Impulse);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
