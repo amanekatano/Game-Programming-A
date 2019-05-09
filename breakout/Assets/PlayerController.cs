941d11c3992b9783cc4e8a94ce5f993f467e9abb
Tomohiko Mukai <tmki@acm.org>
2019-05-09 11:29:38 +0900

    PlayerController

---
+++ breakout/Assets/PlayerController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    Rigidbody2D rb;
+    public float speed = 10.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float vx = Input.GetAxis("Horizontal");
+        float vy = Input.GetAxis("Vertical");
+        rb.velocity = new Vector2(vx, vy) * speed;
+    }
+}
